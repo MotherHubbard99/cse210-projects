fa80d5ba594052da9bed7623dfdf1cf41ae9659e
Angela Hubbard <angiewheeler99@gmail.com>
2024-05-02 23:45:21 -0600

    Week 2 journal. Saving starting changes.

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        //Ask the user what they would like to do
+        Console.WriteLine("Please select one of the following choices: ");
+        Console.WriteLine("1. Write");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Load");
+        Console.WriteLine("4. Save");
+        Console.WriteLine("5. Quit");
+        Console.WriteLine("What would you like to do?");
+        Console.Write("> ");
+        string promptGen = Console.ReadLine();
+        
+        if (promptGen == "1")
+            {
+               PromptGenerator myPrompt = new PromptGenerator();
+            }
+        else if (promptGen == "2")
+            {
+
+            }
+        else if (promptGen == "3")
+            {
+                
+            }
+        else if (promptGen == "4")
+            {
+
+            }
+        else if (promptGen == "5")
+            {
+
+            }
+        else
+            Console.WriteLine("Your selection needs to be 1, 2, 3, 4, or 5.  Please try again. ");
+        
+
+
     }
 }
--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -1,8 +1,22 @@
+using System.Runtime.CompilerServices;
+using System.Collections.Generic;
+
 public class PromptGenerator
 {
     public List<string> _prompt;
     public string GetRandomPrompt()
     {
-        return "";
+        _prompt = new List<string>
+        {
+            "Who was the most intersting person I interacted with today? ",
+            "What was the best part of my day? ",
+            "How did I see the hand of the Lord in my life today? ",
+            "If I had one thing I could do over today, what would it be? "
+
+        };
+        
+        Random random = new Random();
+        int randomIndex = random.Next(_prompt.Count);
+        return _prompt[randomIndex];
     }
 }
